6d62f2a19f064525da3d11d683e3fd86c8e2c7d1
Dave G. <davegeerardyn@gmail.com>
2015-04-30 11:45:22 +0200

    Update sencha-touch-debug.js
    Update uncaught reference error, according to http://stackoverflow.com/questions/27040297/uncaught-referenceerror-webkitpoint-is-not-defined

--- sencha-touch-1.1.0/sencha-touch-debug.js
+++ sencha-touch-1.1.0/sencha-touch-debug.js
@@ -11609,8 +11609,8 @@ Ext.Element.addMethods({
     
     getXY : function() {
         
-        var point = window.webkitConvertPointFromNodeToPage(this.dom, new WebKitPoint(0, 0));
-        return [point.x, point.y];
+        var rect = this.dom.getBoundingClientRect();
+	return [rect.left, rect.top];
     },
 
     
